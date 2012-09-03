6e8e56ea84b744182ce4ed88019b54f07ef747d2
Gido Manders <gido@gamebasics.nl>
2012-09-03 11:39:42 +0200

    Start view added

---
+++ iStory/iStory/Controller/StartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartViewController.h
+//  iStory
+//
+//  Created by Gido Manders on 03-09-2012.
+//  Copyright (c) 2012 HSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartViewController : UIViewController
+
+@end
