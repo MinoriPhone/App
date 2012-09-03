48878b1a42ec14a1a6c1c72298d7611df3aacd8c
Gido Manders <gido@gamebasics.nl>
2012-09-03 15:08:40 +0200

    Story view added

---
+++ iStory/iStory/Controller/StoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryViewController.h
+//  iStory
+//
+//  Created by Gido Manders on 03-09-2012.
+//  Copyright (c) 2012 HSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryViewController : UIViewController
+
+@end
---
+++ iStory/iStory/Controller/StoryViewController.m
@@ -0,0 +1,44 @@
+//
+//  StoryViewController.m
+//  iStory
+//
+//  Created by Gido Manders on 03-09-2012.
+//  Copyright (c) 2012 HSS. All rights reserved.
+//
+
+#import "StoryViewController.h"
+
+@interface StoryViewController ()
+
+@end
+
+@implementation StoryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
